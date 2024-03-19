2a4e1962bc1368782e4cb5dff75fa4395747b093
hernan <hernan@correo.com>
2024-03-19 17:01:48 -0300

    print

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import dht, machine
 
-d = dht.DHT22(machine.Pin(13))
+d = dht.DHT22(machine.Pin(23))
 d.measure()
 d.temperature()
 d.humidity()
+
+temperatura=d.temperature()
+print(f"la temperatura actual es de {temperatura} *C")
+
+humedad=d.humidity()
+
+print(f"la humedad actual es de {humedad} %")
